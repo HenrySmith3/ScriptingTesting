f5c56eb615e783c85d167c4a1fbf67d5f9e965d4
HSmith3 <henry.smith.athena@gmail.com>
2015-07-28 23:32:46 -0400

    Initial commit, it's a 3x3 grid of cubes.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public GameObject obj;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				Instantiate (obj, new Vector3((i-1)*2,(j-1)*2,0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
